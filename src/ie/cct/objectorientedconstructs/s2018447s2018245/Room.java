8ccf74ffb0f39094ea310486433cd232ee13127e
wfgv <willian.voelz@gmail.com>
2020-03-10 10:10:48 +0000

    createAvailability + Room Class Parameters

--- src/ie/cct/objectorientedconstructs/s2018447s2018245/Room.java
+++ src/ie/cct/objectorientedconstructs/s2018447s2018245/Room.java
@@ -1,10 +1,40 @@
 package ie.cct.objectorientedconstructs.s2018447s2018245;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import ie.cct.objectorientedconstructs.interfaces.RoomInterface;
 
 public class Room implements RoomInterface {
+	
+	private String type;
+	private double rate;
+	private Map<String, boolean[]>availability;
+	private int id;
+	
+	public Room(String type, double rate, int id) {
+		this.type = type;
+		this.rate = rate;
+		this.id = id;
+		this.availability = new HashMap<String, boolean[]>();
+		createAvailability();
+	}
+	
+	private void createAvailability() {
+		
+		availability.put("January", new boolean[31]);
+		availability.put("February", new boolean[28]);
+		availability.put("March", new boolean[31]);
+		availability.put("April", new boolean[30]);
+		availability.put("May", new boolean[31]);
+		availability.put("June", new boolean[30]);
+		availability.put("July", new boolean[31]);
+		availability.put("August", new boolean[31]);
+		availability.put("September", new boolean[30]);
+		availability.put("October", new boolean[31]);
+		availability.put("November", new boolean[30]);
+		availability.put("December", new boolean[31]);
+	}
 
 	@Override
 	public String getType() {
